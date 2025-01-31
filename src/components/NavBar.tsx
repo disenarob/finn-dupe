03120ffdb026049b149395c508ba4f26fdc7309e
disenarob <mail@disena.no>
2025-01-31 21:15:40 +0100

    Added dummy data in navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,17 @@
+import '../styles/NavBar.css';
+
 function NavBar() {
   return (
-    <nav>
-      <span></span>
+    <div className='menu-container'>
+    <nav className='main-menu'>
+      <span className='main-menu-title'>Finn.no</span>
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li>meun item 1</li>
+        <li>meun item 1</li>
+        <li>meun item 1</li>
       </ul>
     </nav>
+    </div>
   );
 }
 
